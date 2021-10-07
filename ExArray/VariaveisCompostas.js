829a07fabf333647310ad3e84b8975acaaa6dc5b
vininatus21 <vini.natus21@gmail.com>
2021-10-07 10:29:54 -0300

    feito

--- ExArray/VariaveisCompostas.js
+++ ExArray/VariaveisCompostas.js
@@ -1,11 +1,12 @@
 function clicar() {
-    var valores = [1581, 5, 6]
-    var valores2 = [4, 1, 1581]
+    var valores = [4, 2, 3]
+    var valores2 = [2, 1, 4]
+    var guardar = valores
+    var guardar2 = valores2
     for (var val = 0; val < valores.length; val++) {
         for (var val2 = 0; val2 < valores2.length; val2++) {
-            if (valores[val] == valores2[val2]) {
-                alert(valores[val] + valores2[val2])
-            }
+            if (valores[val] == valores2[val2]) {}
         }
     }
+    alert(parseInt(guardar) + parseInt(guardar2))
 }
