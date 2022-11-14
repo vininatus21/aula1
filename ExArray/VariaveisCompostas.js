d912bb2968f58dcb08b9fbafa998bc12503806ff
vininatus21 <vini.natus21@gmail.com>
2022-11-14 10:20:16 -0300

    ajustes

--- ExArray/VariaveisCompostas.js
+++ ExArray/VariaveisCompostas.js
@@ -1,12 +1,13 @@
 function clicar() {
     var valores = [4, 2, 3]
     var valores2 = [2, 1, 4]
-    var guardar = valores
-    var guardar2 = valores2
+    var Somar = 0;
     for (var val = 0; val < valores.length; val++) {
         for (var val2 = 0; val2 < valores2.length; val2++) {
-            if (valores[val] == valores2[val2]) {}
+            if (valores[val] == valores2[val2]) {
+                Somar = Somar + valores[val]
+            }
         }
     }
-    alert(parseInt(guardar) + parseInt(guardar2))
+    alert(Somar)
 }
---
+++ ExArray/VariaveisCompostas3.js
@@ -0,0 +1,12 @@
+function VerificarReal() {
+    var Arr1 = [250100.0]
+    var Arr2 = [250100.0]
+    for (var valArr1 = 0; valArr1 < Arr1.length; valArr1++) {
+        for (var valArr2 = 0; valArr2 < Arr2.length; valArr2++) {
+            if (Arr1[valArr1] = Arr2[valArr2]) {
+
+            }
+        }
+    }
+
+}
